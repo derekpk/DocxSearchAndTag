d8b2a0e5f3dac3c9144dd8e0e46ee057752fc1ef
derekpk <derekpkeogh@gmail.com>
2015-07-29 15:23:10 +0100

    Added fileChooser to main and a few minor changes to the example sequence file

--- src/ie/docoder/docx/searcher/Main.java
+++ src/ie/docoder/docx/searcher/Main.java
@@ -7,23 +7,42 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
 
+	  private void showOpenFileDialog() {
+	        JFileChooser fileChooser = new JFileChooser();
+	        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+	        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("PDF Documents", "pdf"));
+	        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("MS Office Documents", "docx", "xlsx", "pptx"));
+	        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Images", "jpg", "png", "gif", "bmp"));
+	        fileChooser.setAcceptAllFileFilterUsed(true);
+	        int result = fileChooser.showSaveDialog(fileChooser);
+	        if (result == JFileChooser.APPROVE_OPTION) {
+	            File selectedFile = fileChooser.getSelectedFile();
+	            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+	        }
+	    }
 	public static void main(String[] args) {
 
-		String docxFilename = args[0];
-		String xmlFilename = args[1];
-		for (String s: args) {
-			System.out.println(s);
-	    }
+		JFileChooser chooser = new JFileChooser();
 		
-//		JFileChooser chooser = new JFileChooser();
-//		FileNameExtensionFilter filter = new FileNameExtensionFilter("Microsoft Docx files", "docx");
-//	    chooser.setFileFilter(filter);
-//	    int returnVal = chooser.showOpenDialog(chooser);
-//	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-//	       System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
-//	    }
-		BlobFinder finder = new BlobFinder(true);
-//		finder.ShowMessage(docxFilename);		
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Microsoft Docx files", "docx");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(chooser);
+		String docxFilename = null;
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	    	docxFilename = chooser.getSelectedFile().getPath();
+	    	System.out.println("You chose to search this file: " + docxFilename);
+	    }
+		filter = new FileNameExtensionFilter("Sequence Definition files", "xml");
+	    chooser.setFileFilter(filter);
+	    returnVal = chooser.showOpenDialog(chooser);
+		String xmlFilename = null;
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	    	xmlFilename = chooser.getSelectedFile().getPath();
+	    	System.out.println("With this sequence file: " + xmlFilename);
+	    }
+
+	    BlobFinder finder = new BlobFinder(true);
 		finder.ShowMessage("DOCX Searcher");
 	
 		finder.Search(docxFilename, xmlFilename, null);
